5d96842b7f6a79304a36bf39ba0f6c7167218c6e
Thaina-ADS <thainarodriguesmiranda@gmail.com>
2025-01-14 16:13:23 -0300

    program

--- program.cs
+++ program.cs
@@ -1,1 +1,106 @@
-joij
+// 1- Efetuar a leitura de dois valores numéricos inteiros representados pelas variáveis A e B e apresentar o resultado da diferença do maior valor pelo menor valor.
+// PASSOS
+// 1. DoisValores : ValorA, ValorB
+
+//*Usuario pode ter: Id, Nome, Email, Senha, Salario, Celular, Data de cadastro, Data de atualização
+// *     Users:            Id, Name, Email, Password, Salary, Phone, CreatedAt, UpdatedAt
+
+// Pets : Id, Nome, Raça, Espécie, Sexo, Idade
+
+
+
+using AtividadesLivro108109.Models;
+
+// INICIO ATIVIDADE 7 ------------------------------------------------------------------
+
+
+
+// FIM ATIVIDADE 7 ---------------------------------------------------------------------
+
+
+
+
+// INICIO ATIVIDADE 6 ------------------------------------------------------------------
+
+//ValoresCrescentes valores = new ValoresCrescentes(40, 20, 10, "bianca", "anabel", "daniela");
+//int[] idades = valores.IdadesCrescentes(valores.Quarenta, valores.Vinte, valores.Dez);
+//Console.WriteLine($"Idade em ordem crescente:");
+//foreach (int idade in idades)
+//{
+//    Console.WriteLine(idade);
+//}
+
+//string[] nomes = valores.NomesCrescentes(valores.Bia, valores.Ana, valores.Dani);
+//Console.WriteLine($"Nomes em ordem alfabética:");
+//foreach (string nome in nomes)
+//{
+//    Console.WriteLine(nome);
+//}
+
+// FIM ATIVIDADE 6 ---------------------------------------------------------------------
+
+
+// INICIO ATIVIDADE 5 ------------------------------------------------------------------
+
+//EquacaoSegundoGrau equacao = new EquacaoSegundoGrau(1, -3, 2);
+//string resultado = equacao.CalcularBhaskara(equacao.A, equacao.B, equacao.C);
+//Console.WriteLine(resultado);
+
+// FIM ATIVIDADE 5 ---------------------------------------------------------------------
+
+
+// INICIO ATIVIDADE 4 ------------------------------------------------------------------
+
+// obs. faltou exame
+
+//CalculandoMedia calculandoMedia = new CalculandoMedia(6, 5, 5, 5);
+//Console.WriteLine(calculandoMedia);
+
+//decimal mediaInicial = calculandoMedia.MediaFinal1(calculandoMedia.Nota1, calculandoMedia.Nota2, calculandoMedia.Nota3, calculandoMedia.Nota4);
+//Console.WriteLine($"Sua média é de: {mediaInicial}");
+
+//string resultadoMedia = calculandoMedia.VerificaAprovacao(calculandoMedia.Nota1, calculandoMedia.Nota2, calculandoMedia.Nota3, calculandoMedia.Nota4);
+//Console.WriteLine($"Média final de: {resultadoMedia}");
+
+// FIM ATIVIDADE 4 ---------------------------------------------------------------------
+
+// INICIO ATIVIDADE 3 ------------------------------------------------------------------
+
+//MediaAluno mediaFinal = new MediaAluno("Maria Oliveira", 2, 5, 7, 9);
+
+//decimal mediaAluno = mediaFinal.ObterMediaAluno(mediaFinal.Nota1, mediaFinal.Nota2, mediaFinal.Nota3, mediaFinal.Nota4);
+//string statusAprovacao = mediaFinal.VerificarAprovacao(mediaFinal.Nota1, mediaFinal.Nota2, mediaFinal.Nota3, mediaFinal.Nota4);
+//Console.WriteLine($"O aluno {mediaFinal.Name} foi {statusAprovacao} com média de: {mediaAluno}");
+
+// FIM ATIVIDADE 3 ------------------------------------------------------------------
+
+
+// INICIO ATIVIDADE 2 ------------------------------------------------------------------
+//ValoresPositivoNegativo resultado = new ValoresPositivoNegativo(-28);
+
+//Console.WriteLine($"O primeiro valor é: {resultado.ValorVariavelN}");
+//int resultadoValor = resultado.TornarValorPositivo(resultado.ValorVariavelN);
+//Console.WriteLine($"O valor final é igual a: {resultadoValor}");
+
+// FIM ATIVIDADE 2 ----------------------------------------------------------------------
+
+
+
+
+
+// INICIO ATIVIDADE 1 -------------------------------------------------------------------
+
+//ValoresUm informacoes = new ValoresUm(82, 13);
+
+//// Exibindo os valores A e B
+//Console.WriteLine($"Valor A: {informacoes.ValorA}");
+//Console.WriteLine($"Valor B: {informacoes.ValorB}");
+
+//// Calculando a subtração e exibindo a diferença com mensagem personalizada
+//int resultadoSubtracao = informacoes.Subtracao(informacoes.ValorA, informacoes.ValorB);
+//Console.WriteLine($"A diferença entre os valores {informacoes.ValorA} e {informacoes.ValorB} é: {resultadoSubtracao}");
+
+// FIM ATIVIDADE 1 ---------------------------------------------------------------------
+
+
+
